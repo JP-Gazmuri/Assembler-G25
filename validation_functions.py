293f194afba776739b050e9695a9a89352062689
JP-Gazmuri <jpgazmuri@miuandes.cl>
2021-10-04 00:15:54 -0300

    ultima correccion hoydia

--- validation_functions.py
+++ validation_functions.py
@@ -1,12 +1,20 @@
 def validate_labels(instructions):
     inlabels = []
     outlabels = []
+    outlabels_positions = []
+    i = 0
+    correct = True
     for instruction in instructions:
         if instruction['inlabel'] != "":
             inlabels.append(instruction['inlabel'])
         if instruction['outlabel'] != "":
             outlabels.append(instruction['outlabel'])
+            outlabels_positions.append(i)
+        i+=1
+    i = 0
     for label in outlabels:
         if label not in inlabels:
-            return False
-    return True
+            print(f"La etiqueta usada en linea {i} {label} no es declarada")
+            correct = False
+        i+=1
+    return correct
