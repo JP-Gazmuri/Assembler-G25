4baf4280ad74b2e2d7644437808d91f4d73ea6a2
JP-Gazmuri <jpgazmuri@miuandes.cl>
2021-10-05 01:02:38 -0300

    assembler V0.1
    entrega 1 terminada

--- Assembler.py
+++ Assembler.py
@@ -2,9 +2,9 @@ from validation_functions import *
 from reading_functions import *
 
 
-file = input("escriba el nombre del archivo (en el mismo directorio) a revisar: \n")
+readfile = input("escriba el nombre del archivo (con extension y en el mismo directorio) a revisar: \n")
 start_of_CODE = 1
-file = open("Example.ass", "r", encoding = "utf-8")
+file = open(readfile, "r", encoding = "utf-8")
 instructions = []
 
 for line in file:
